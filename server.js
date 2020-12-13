15fb49bc45b39e3924b6cbc22f6f32f59a7303a2
Corentin Biguier <cbiguier.ir2021@esaip.org>
2020-12-13 17:54:37 +0100

    up

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-var fs = require("fs");
-var express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var bodyParser = require("body-parser");
-var io = require("socket.io").listen(server);
+var fs 			= require("fs");
+var express 	= require('express');
+var app 		= express();
+var server 		= require('http').Server(app);
+var bodyParser 	= require("body-parser");
+var io 			= require("socket.io").listen(server);
 
-var Juego = require("./Servidor/Juego/Juego.js");
+var Juego 	= require("./Servidor/Juego/Juego.js");
 var Usuario = require("./Servidor/Juego/Usuario/Usuario.js")
-var wss = require("./Servidor/servidorWS.js");
+var wss 	= require("./Servidor/servidorWS.js");
 
+
+//Is to use in the server
 app.set('port', process.env.PORT || 5000);
 
 app.use(express.static(__dirname + '/'));
@@ -18,25 +20,25 @@ app.use(bodyParser.json());
 var juego = new Juego();
 
 app.get('/', function (request, response) {
-    var contenido = fs.readFileSync(__dirname + "/Cliente/index.html"); 
+    var contenido = fs.readFileSync(__dirname + "/cliente/index.html"); 
     response.setHeader("Content-type", "text/html");
     response.send(contenido);
 });
 
 app.get("/juego",function(request,response){
-	var contenido = fs.readFileSync(__dirname + "/Cliente/game2d/index-game.html"); 
+	var contenido = fs.readFileSync(__dirname + "/cliente/game2d/index-game.html"); 
     response.setHeader("Content-type", "text/html");
     response.send(contenido);
 });
 
-server.listen(app.get('port'), function () {
-    console.log('Node app is running on port', app.get('port'));
-});
+ server.listen(app.get('port'), function () {
+ 	console.log('Node app is running on port', app.get('port'));
+ });
 
-var servidorWS = new wss(io);
-servidorWS.lanzarSocketSrv(servidorWS,juego);
+ var servidorWS = new wss(io);
+ servidorWS.lanzarSocketSrv(servidorWS,juego);
 
-
+//To use in local
 /*app.get("/crearPartida/:num/:nick",function(request,response){
 	let owner = request.params.nick;
 	let num = request.params.num;
